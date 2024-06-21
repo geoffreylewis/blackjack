2b41d9139e84c2680d071468c68b25ea3cb03e1d
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-06-21 11:52:06 -0500

    Add files via upload
    Starting project...

---
+++ 100_Days_Day_11.py
@@ -0,0 +1,31 @@
+#############
+# Blackjack #
+#############
+
+# ASCII art for beginning banner/logo (courtesy of https://replit.com/@appbrewery/blackjack-final#art.py).
+banner_logo = """
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+"""
+
+# Intro to Blackjack.
+print(banner_logo)
+print("Do you want to play a game of Blackjack?  Type \"y\" or \"n\": \n")
+
+# print("Your cards: ")
+# print("Computer's first card: ")
+# print("Type \"y\" to get another card, or type \"n\" to pass: ")
+
+# print("Your final hand: ")
+# print("Computer's final hand: ")
+# print("You win!")
+
+# print("Do you want to play a game of Blackjack?  Type \"y\" or \"n\": \n")
+
+# print("Good game!")
